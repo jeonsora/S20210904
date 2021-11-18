865d81c5ae7e33d1c9abe11b473fb76285ecc467
Jung0310 <92971228+Jung0310@users.noreply.github.com>
2021-11-18 11:13:40 +0900

    test1
    1

--- sBootMybatis03/src/main/java/com/oracle/sBootMybatis03/dao/MemberJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oracle.sBootMybatis03.dao;
-
-import java.util.List;
-
-import com.oracle.sBootMybatis03.domain.Member;
-
-public interface MemberJpaRepository {
-	List<Member> findAll();
-}
