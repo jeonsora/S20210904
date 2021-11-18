53cd1f975461aa4c39748129e2a4adaeb6e8b5f4
parkds93 <8757279@naver.com>
2021-11-18 14:30:48 +0900

    bone

---
+++ s20210904/src/main/java/com/oracle/s20210904/ds/TestController.java
@@ -0,0 +1,21 @@
+package com.oracle.s20210904.ds;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping(value="testForm")
+	public String testForm() {
+		return "testForm";
+	}
+	
+	@GetMapping(value="test")
+	public String test(String id,Model model) {
+		String id2 = id;
+		model.addAttribute("id",id2);
+		return "test";
+	}
+}
