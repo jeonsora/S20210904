eec8da7259897da6eda387645986818ea2780968
Jung0310 <92971228+Jung0310@users.noreply.github.com>
2021-11-18 12:37:41 +0900

    1
    1

--- s20210904/src/main/java/com/oracle/s20210904/comm/model/ResumeContect.java
+++ s20210904/src/main/java/com/oracle/s20210904/comm/model/ResumeContect.java
@@ -1,8 +1,11 @@
 package com.oracle.s20210904.comm.model;
 
+import java.io.Serializable;
 import java.sql.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -10,11 +13,19 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
+@Entity(name = "ResumeContect")
 @Table(name = "resume_contect")
-public class ResumeContect {
+@IdClass(ResumeContect.class)
+public class ResumeContect implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
 	private String com_id;
+	@Id
 	private String user_id;
+	@Id
 	private String res_code;
 	private Date res_date;
 	private String ntc_code;
