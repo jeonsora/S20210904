2ead4cad9a94f26104275bb57adde805c1dfb615
mingyeongmin <93586051+min-gyeong-min@users.noreply.github.com>
2021-11-25 16:33:05 +0900

    GM commit1

--- s20210904/src/main/java/com/oracle/s20210904/comm/model/ComAnnounce.java
+++ s20210904/src/main/java/com/oracle/s20210904/comm/model/ComAnnounce.java
@@ -5,9 +5,25 @@ import java.sql.Date;
 import lombok.Getter;
 import lombok.Setter;
 
+// JOIN 
 @Getter
 @Setter
-public class Announce {
+public class ComAnnounce {
+	
+	// company
+	private String com_pw;
+	private String com_name;
+	private String com_tel;
+	private String com_addr;
+	private String com_email;
+	private String com_sec;
+	private String com_bus;
+	private String com_regnum;
+	private String com_mgr_tel;
+	private String com_web;
+	private String com_img;
+	
+	// announce
 	private String anno_code;
 	private String com_id;
 	private String work_code;
@@ -17,7 +33,7 @@ public class Announce {
 	private int    emp_type;
 	private String career;
 	private String rec_edu;
-	private int pay_info;
+	private int    pay_info;
 	private String work_cdt;
 	private String etc_cdt;
 	private String job_tag;
@@ -25,12 +41,16 @@ public class Announce {
 	private String tech_tag2;
 	private Date   anno_regdate;
 	private Date   anno_c_regdate;
-	private String anno_title;
+	private String anno_title; 
+	
+	
+	
+	
 	
 	// 조회용
 	private String pageNum;
 	private int start;
 	private int end;
+	
+
 }
-
-
